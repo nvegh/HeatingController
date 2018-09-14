915d600da317cd2150871f2a3306e121478477d0
nvegh <mr.norbert.vegh@gmail.com>
2018-09-14 18:49:25 +0200

    Auto logic

--- arduino.heatingcontroller/arduino.heatingcontroller.ino
+++ arduino.heatingcontroller/arduino.heatingcontroller.ino
@@ -16,10 +16,11 @@ MAX6675_Thermocouple* thermocouple2 = NULL;   //fireplace
 
 double water_temp = 0.0;
 double fireplace_temp = 0.0;
-boolean relay_switch = LOW;
+boolean pumpRelay = LOW;
+boolean auto_trigger = LOW;
+boolean waterHot_trigger = LOW;
 
-double filteredReading = 0;
-
+unsigned long auto_trigger_time = 0;
 
 void setup() {
   u8g.setColorIndex(1); // Instructs the display to draw with a pixel on. 
@@ -28,9 +29,9 @@ void setup() {
   thermocouple2 = new MAX6675_Thermocouple(thermo2_SCK_PIN, thermo2_CS_PIN, thermo2_SO_PIN, 15, 7);
 
   water_temp = thermocouple1->readCelsius();
-  //fireplace_temp = thermocouple2->readCelsius();
+  fireplace_temp = 40; //thermocouple2->readCelsius();
 
-  Timer1.initialize(10000000);  //1sec - 1000000
+  Timer1.initialize(5000000);  //1sec - 1000000
   Timer1.attachInterrupt(controlIt);
 
   pinMode(LED_BUILTIN, OUTPUT);
@@ -51,22 +52,62 @@ void loop() {
 
 void controlIt()
 {
-  readTemps(); 
+  readTemps();
 
-  Serial.println(water_temp);
-  //water_temp = water_temp + 0.5;
-
-
-  //logic to switch the relay on
-  relay_switch = water_temp > 30;
+  fireplace_temp = fireplace_temp + 0.4;
   
-  digitalWrite(LED_BUILTIN, relay_switch);
+  if (fireplace_temp >= 42 && auto_trigger == LOW && pumpRelay == LOW){
+    auto_trigger = HIGH;
+    auto_trigger_time = millis();
+    Serial.println("auto triggered");  
+    delay(5);
+  }
+
+  if (auto_trigger == HIGH) { 
+    if (millis()-auto_trigger_time < 600000) {  //10 minutes
+      Serial.println("auto trigger time ON");  
+      if (fireplace_temp >= 50) {
+          Serial.println("RELAY ON!!");
+          auto_trigger = LOW;
+          pumpRelay = HIGH;
+        }
+    }
+    else {
+            auto_trigger = LOW;
+            Serial.println("auto trigger time OFF");
+         }
+  }
+
+  if (pumpRelay == HIGH && water_temp >= 30) { 
+      waterHot_trigger = HIGH;
+        Serial.println("waterHot_trigger HIGH");
+  }
+  
+  if (waterHot_trigger == HIGH && water_temp <= 20) { 
+        pumpRelay = LOW;
+        waterHot_trigger = LOW;
+        Serial.println("PUMP OFF!!");
+  }
+
+  Serial.print("Water: ");
+  Serial.print(water_temp);
+  Serial.print(", Fire: ");
+  Serial.print(fireplace_temp);
+  Serial.print(", auto_trigger: ");
+  Serial.print(auto_trigger);
+  Serial.print(", auto_trigger time: ");
+  Serial.print(millis()-auto_trigger_time);
+  Serial.print(", pumpRelay: ");
+  Serial.println(pumpRelay);  
+  
+   
+  digitalWrite(LED_BUILTIN, pumpRelay);
 }
 
 void readTemps()
 {
   water_temp = .85 * water_temp + .15 * thermocouple1->readCelsius();
-   //fireplace_temp = 0.85 * fireplace_temp + .15 * thermocouple2->readCelsius();
+  //fireplace_temp = 0.85 * fireplace_temp + .15 * thermocouple2->readCelsius();
 }
   
 void draw(){
@@ -92,7 +133,7 @@ void draw(){
 
   
   u8g.setFont(u8g_font_profont15);
-  if (relay_switch) u8g.drawStr(0, 9, "PUMPA");
+  if (pumpRelay) u8g.drawStr(0, 9, "PUMPA");
 
   u8g.drawStr(128-u8g.getStrWidth(cFT), 9, cFT);
 }
