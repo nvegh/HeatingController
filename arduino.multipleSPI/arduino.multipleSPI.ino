3a0808ae86e101e2be68116037087e9f162f506c
nvegh <mr.norbert.vegh@gmail.com>
2018-09-29 21:15:18 +0200

    Multiple SPI clients test

--- arduino.multipleSPI/arduino.multipleSPI.ino
+++ arduino.multipleSPI/arduino.multipleSPI.ino
@@ -4,6 +4,9 @@
 
 #define SPI_1 9
 #define SPI_2 10
+#define SetPin 2
+#define ResetPin 3
+
 //Arduino SCK (pin 13) to all the SCK pins.
 //Arduino MISO (pin 12) to all the DO (SO) pins
 
@@ -35,13 +38,32 @@ void setup() {
     digitalWrite(SPI_1, HIGH);
     digitalWrite(SPI_2, HIGH);
 
+
+    pinMode(SetPin, OUTPUT);
+    pinMode(ResetPin, OUTPUT);
+    digitalWrite(SetPin, LOW);
+    digitalWrite(ResetPin, LOW);
+    
     Serial.begin(115200);
 }
 
 void loop() {
-    Serial.print(readCelsius(SPI_1));
-    Serial.print(" ");
-    Serial.println(readCelsius(SPI_2));
-    delay(1000);
+//    Serial.print(readCelsius(SPI_1));
+//    Serial.print(" ");
+//    Serial.println(readCelsius(SPI_2));
+//    delay(1500);
+
+digitalWrite(SetPin, HIGH);
+delay(100);
+digitalWrite(SetPin, LOW);
+
+delay(8000);
+
+
+digitalWrite(ResetPin, HIGH);
+delay(100);
+digitalWrite(ResetPin, LOW);
+
+delay(8000);
 }
 
